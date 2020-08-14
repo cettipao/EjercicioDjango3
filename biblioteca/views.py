2ed24e679a6eff9eb47f0140a51c39dc1b9f5209
cettipao <cettipao@live.com.ar>
2020-08-14 15:25:14 -0300

    Dinamic Template, Bucle For

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Libro,Revista
 
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['libros'] = Libro.objects.all()
+        context['revistas'] = Revista.objects.all()
+        return context
